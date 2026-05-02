82a47c76cd9c753c9647c3985cc31189a160291f
aabathorat11 <aabathorat11@gmail.com>
2026-05-02 20:37:19 +0530

    Update backend.tf

--- 1-network/backend.tf
+++ 1-network/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "quantamvector-infra-statefile-backups"
     key            = "quantamvector/1-network/terraform.tfstate"
-    region         = "ap-northeast-1"
+    region         = "us-east-1"
     dynamodb_table = "quantamvector-terraform-locks"
     encrypt        = true
   }
